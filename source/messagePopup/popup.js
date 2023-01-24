8632f5c209b0fd404ccf16454b62ade95a50e5e3
Sebastian Wieland <git-commits@nwie.land>
2023-01-24 20:05:33 +0100

    Minor changes for mozilla's automatic review

--- source/messagePopup/popup.js
+++ source/messagePopup/popup.js
@@ -1,18 +1,20 @@
-let tabs = await messenger.tabs.query({ active: true, currentWindow: true });
+(async () => {
+  let tabs = await messenger.tabs.query({ active: true, currentWindow: true });
 
-let message = await messenger.messageDisplay.getDisplayedMessage(tabs[0].id);
-let full = await messenger.messages.getFull(message.id);
+  let message = await messenger.messageDisplay.getDisplayedMessage(tabs[0].id);
+  let full = await messenger.messages.getFull(message.id);
 
-if (!full.headers.hasOwnProperty("x-simplelogin-type") || full.headers["x-simplelogin-type"][0] !== "Forward") {
-  document.querySelector("body").classList.add("not-forwarded");
-} else {
-  let from = full.headers.hasOwnProperty("x-simplelogin-envelope-from") ? full.headers["x-simplelogin-envelope-from"][0] : "unknown";
-  let to = full.headers["x-simplelogin-envelope-to"][0];
-  let deliveredTo = full.headers["delivered-to"][0];
-  let unsubscribeHref = full.headers['list-unsubscribe'][0].replace(/^<(.*)>$/, '$1');
-  
-  document.querySelector("#from").textContent = from;
-  document.querySelector("#to").textContent = to;
-  document.querySelector("#deliveredTo").textContent = deliveredTo;
-  document.querySelector("#unsubscribe").href = unsubscribeHref + "&body=Send this e-mail to disable the alias <" + to + ">";
-}
+  if (!full.headers.hasOwnProperty("x-simplelogin-type") || full.headers["x-simplelogin-type"][0] !== "Forward") {
+    document.querySelector("body").classList.add("not-forwarded");
+  } else {
+    let from = full.headers.hasOwnProperty("x-simplelogin-envelope-from") ? full.headers["x-simplelogin-envelope-from"][0] : "unknown";
+    let to = full.headers["x-simplelogin-envelope-to"][0];
+    let deliveredTo = full.headers["delivered-to"][0];
+    let unsubscribeHref = full.headers['list-unsubscribe'][0].replace(/^<(.*)>$/, '$1');
+
+    document.querySelector("#from").textContent = from;
+    document.querySelector("#to").textContent = to;
+    document.querySelector("#deliveredTo").textContent = deliveredTo;
+    document.querySelector("#unsubscribe").href = unsubscribeHref + "&body=Send this e-mail to disable the alias <" + to + ">";
+  }
+})();
